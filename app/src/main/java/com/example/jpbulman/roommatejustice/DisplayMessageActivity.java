c1a31ba05bfdb1a180a88997dee4f362e162444c
jpthesolver2 <jpbulman@wpi.edu>
2018-01-12 20:59:36 -0500

    Basic activities

--- app/src/main/java/com/example/jpbulman/roommatejustice/DisplayMessageActivity.java
+++ app/src/main/java/com/example/jpbulman/roommatejustice/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.jpbulman.roommatejustice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = new Intent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
--- app/src/main/java/com/example/jpbulman/roommatejustice/MainActivity.java
+++ app/src/main/java/com/example/jpbulman/roommatejustice/MainActivity.java
@@ -10,6 +10,8 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
